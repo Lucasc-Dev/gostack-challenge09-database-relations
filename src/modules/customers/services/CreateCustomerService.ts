b309aaab06f13099afc276951f13f2dd96f6e407
Lucas César <lucasambrogi1@hotmail.com>
2020-08-24 20:16:15 -0300

    Customers route done.

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findEmail = await this.customersRepository.findByEmail(email);
+
+    if (findEmail) {
+      throw new AppError('Email already registered');
+    }
+
+    const customer = await this.customersRepository.create({
+      email, name,
+    });
+
+    return customer;
   }
 }
 
